feeda98f1dafd77a9b625092d822a0721e98dfe0
Suraj Meshram <104337388+surajmesh@users.noreply.github.com>
2023-10-16 21:49:28 +0530

    programe PlusMinus
    calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with places after the decimal.
    
    Signed-off-by: Suraj Meshram <104337388+surajmesh@users.noreply.github.com>

--- Python/PlusMinus.py
+++ Python/PlusMinus.py
@@ -2,7 +2,7 @@
 # Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero.
 # Print the decimal value of each fraction on a new line with  places after the decimal.
 
-# make function for PlusMinus
+# Make function for PlusMinus
 
 def plusMinus(arr):
     positive_numbers = []   # store pos number 
